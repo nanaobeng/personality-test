b323c7ead0a41c336a1311c81eb6b85c067ea40b
nanaobeng <nanaobengmarnu@gmail.com>
2022-08-07 10:55:41 +0000

    Database Setup - Sequalize

--- server/index.js
+++ server/index.js
@@ -4,13 +4,26 @@ const cors = require("cors");
 require('dotenv').config();
 
 
+const sequelize = require("./database/db");
+
+
+
+
+
 app.use(cors());
 app.use(express.json());
 
 
-const port = process.env.PORT || 8000;
+const main = async () => {
+    
+    
+    const port = process.env.PORT || 8000;
+    app.listen(port, () => {
+      console.log(`SERVER RUNNING ON PORT ${port}`);
+    });
+  };
 
 
-app.listen(port, () => {
-    console.log(`server is running on port ${port}`)
-})
+main().catch((err) => {
+  console.log(err);
+});
